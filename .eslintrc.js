2fe7c3533ea90a04583d255d93576adcc3e4940c
Truong Minh Tien <145823751+marcus1823@users.noreply.github.com>
2025-05-02 15:07:39 +0700

    Add ESLint rules and create initial structure for project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    "semi": "off",
+    "quotes": "off",
+    "comma-dangle": "off",
+    "no-unused-vars": "off",
+    "no-trailing-spaces": "off",
+    "no-multiple-empty-lines": "off",
+    "eol-last": 0,
+  },
 };
---
+++ src/apis/rootApi.ts
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+
+// const rootApi = axios.create({
+//     // baseURL: 'https://2025.cho.vin/api/v1',
+//     baseURL: 'https://brightnest.brightlab.vn/api/v1/',
+// })
+
+// export default rootApi;
